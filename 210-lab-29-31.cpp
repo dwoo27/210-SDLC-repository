b8741c113b4d19d4a0ebed112026ded45a200e4e
dwoo27 <dwooed27@gmail.com>
2026-04-15 20:09:31 -0700

    10 min commit

--- 210-lab-29-31.cpp
+++ 210-lab-29-31.cpp
@@ -11,7 +11,23 @@ using namespace std;
 
 int main()
 {
+	//seed rand generater
 
+	//declare boba shop map
+	//key = station, value = array of 3 lists
+
+	//declare pair vectrs for upcoming order pool
+	//orderNames and stationNames
+
+	//create stations in map
+
+	//load all orders from txt file into vectors
+
+	//display beg state of shop
+
+	//run sim
+
+	//display final state of shop
 }
 
 //function definitions:
@@ -46,7 +62,31 @@ int main()
 		//randomly pull some orders from name/type vectors
 		//each order goes into waiting list of its station
 
+			//stop if orders is empty
 
+			//choose one random order left in order pool
+
+			//get chosen order and its matching station
+
+			//place order into waiting ist for that station
+
+			//dislay where order went
+
+			//erase order from both vectors so not resused
+
+
+		//loop through each station
+			//move some wiaint drinks to in progress
+			//move some in progress drinks to completed
+
+				//random num of drinks to start perdiod
+
+				//move drinks from waiting to in progress
+				
+				//rand num drinks that finish period
+
+				//move drinks from in progress to completed
+		
 
 
 
